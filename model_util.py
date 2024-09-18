41b53193e471827e9d2daebf1110b7188c624e4c
Vishal Vishnu Kagade <105672962+Vishalkagade@users.noreply.github.com>
2024-09-18 12:24:12 +0200

    Add files via upload
    updatinf_models

--- model_util.py
+++ model_util.py
@@ -39,14 +39,14 @@ class image_CBR_2(nn.Module):
 
         self.cbs2 = nn.Sequential(convbnrelu(in_channels=in_channels,out_channels=out_channels),
                                     convbnrelu(in_channels=out_channels,out_channels=out_channels))
-        self.max_pool = nn.MaxPool2d(kernel_size=2,stride=2)
+        self.max_pool = nn.MaxPool2d(kernel_size=2,stride=2,return_indices= True)
 
     def forward(self,x,depth_latent_feature):
 
         x = self.cbs2(x)
-        x = self.max_pool(x + depth_latent_feature)
+        x,indices = self.max_pool(x + depth_latent_feature)
 
-        return x
+        return x,indices
     
 class image_CBR_3(nn.Module):
     """
@@ -58,15 +58,15 @@ class image_CBR_3(nn.Module):
         self.cbs3 = nn.Sequential(convbnrelu(in_channels=in_channels,out_channels=out_channels),
                                     convbnrelu(in_channels=out_channels,out_channels=out_channels),#
                                     convbnrelu(in_channels=out_channels,out_channels=out_channels))
-        self.max_pool = nn.MaxPool2d(kernel_size=2,stride=2)
+        self.max_pool = nn.MaxPool2d(kernel_size=2,stride=2,return_indices= True)
         self.dropout = nn.Dropout()
 
     def forward(self,x,depth_latent_feature):
 
         x = self.cbs3(x)
-        x = self.max_pool(x + depth_latent_feature)
+        x,indices = self.max_pool(x + depth_latent_feature)
 
-        return self.dropout(x)
+        return self.dropout(x),indices
 class depth_CBR_2(nn.Module):
     """
     Implementation with 2 CBR block with fusion
@@ -93,7 +93,7 @@ class depth_CBR_3(nn.Module):
         super(depth_CBR_3,self).__init__()
 
         self.cbs3 = nn.Sequential(convbnrelu(in_channels=in_channels,out_channels=out_channels),
-                                    convbnrelu(in_channels=out_channels,out_channels=out_channels),#
+                                    convbnrelu(in_channels=out_channels,out_channels=out_channels),
                                     convbnrelu(in_channels=out_channels,out_channels=out_channels))
         self.max_pool = nn.MaxPool2d(kernel_size=2,stride=2)
         self.dropout = nn.Dropout()
@@ -104,3 +104,67 @@ class depth_CBR_3(nn.Module):
         x = self.dropout(self.max_pool(skip))
 
         return x,skip
+
+class dec_CBS3(nn.Module):#
+    def __init__(self,in_channels,out_channels):
+        super().__init__()
+
+        self.unpool = nn.MaxUnpool2d(kernel_size=2, stride=2)
+
+        self.cbs3 = nn.Sequential(convbnrelu(in_channels=in_channels,out_channels=out_channels),
+                                    convbnrelu(in_channels=out_channels,out_channels=out_channels),#
+                                    convbnrelu(in_channels=out_channels,out_channels=out_channels),
+                                    nn.Dropout())
+    def forward(self,x,indices,output_size):
+        x = self.unpool(x,indices,output_size = output_size)
+        return self.cbs3(x)
+
+class dec_CBS2(nn.Module):#
+    def __init__(self,in_channels,out_channels,dropout = True):
+        super().__init__()
+
+        self.unpool = nn.MaxUnpool2d(kernel_size=2, stride=2)
+
+        self.cbs3 = nn.Sequential(convbnrelu(in_channels=in_channels,out_channels=out_channels),
+                                    convbnrelu(in_channels=out_channels,out_channels=out_channels))
+        if dropout:
+            self.dropout = nn.Dropout()
+    def forward(self,x,indices,output_size):
+        x = self.unpool(x,indices,output_size)
+        return self.cbs3(x)
+    
+class FullyConnected(torch.nn.Module):
+    def __init__(self,
+                    in_features,
+                    out_features,
+                    weight_initializer='kaiming_uniform',
+                    activation_func=torch.nn.LeakyReLU(negative_slope=0.10, inplace=True),
+                    dropout_rate=0.00):
+            super(FullyConnected, self).__init__()
+
+            self.fully_connected = torch.nn.Linear(in_features, out_features)
+
+            if weight_initializer == 'kaiming_normal':
+                torch.nn.init.kaiming_normal_(self.fully_connected.weight)
+            elif weight_initializer == 'xavier_normal':
+                torch.nn.init.xavier_normal_(self.fully_connected.weight)
+            elif weight_initializer == 'xavier_uniform':
+                torch.nn.init.xavier_uniform_(self.fully_connected.weight)
+
+            self.activation_func = activation_func
+
+            if dropout_rate > 0.00 and dropout_rate <= 1.00:
+                self.dropout = torch.nn.Dropout(p=dropout_rate)
+            else:
+                self.dropout = None
+
+    def forward(self, x):
+        x = self.fully_connected(x)
+
+        if self.activation_func is not None:
+            x = self.activation_func(x)
+
+        if self.dropout is not None:
+            return self.dropout(x)
+        else:
+            return x
